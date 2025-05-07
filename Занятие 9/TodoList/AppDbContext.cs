03540a211daad739d589b50b00bfa9bf667956be
qYs1er <daniilkokorin49@gmail.com>
2025-05-07 17:31:17 +0300

    lesson 9/TodoList/AppDbCountext.cs

--- Занятие 9/TodoList/AppDbContext.cs
+++ Занятие 9/TodoList/AppDbContext.cs
@@ -3,6 +3,19 @@
 namespace TodoList
 {
 	public class AppDbContext : DbContext
-	{
-	}
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=todolist.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(t => t.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
 }
